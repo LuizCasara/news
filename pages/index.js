bd08c57a5fa5b3eb02cd8ffbcd0aa314df8b9132
Luiz Claudio Perin Casara <66029330+LuizCasara@users.noreply.github.com>
2025-02-16 18:37:08 +0000

    adicionado scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Home() {
-    return(
-        <>
-            <h1>Olá mundo!</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+    </>
+  );
 }
 
 export default Home;
